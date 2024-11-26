e9fd01898d6d169cff444f3925ebbe6ed255403f
amarusahertian <amaru.shakur.sahertian@gmail.com>
2024-11-26 11:55:41 +0100

    Update game.js
    update game

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ let landWidth = 100;
 let isLanding = false;
 let gameOver = false;
 let crash = false;
+let win = false;
 
 let state = "start";
 
@@ -27,6 +28,8 @@ function draw() {
     gameScreen();
   } else if (state === "result") {
     resultScreen();
+  } else if (state === "win") {
+    winScreen();
   }
 }
 
@@ -75,7 +78,7 @@ function startScreen() {
     }
     endShape(CLOSE);
   }
-// text start page 
+  // text start page
   textSize(60);
   textAlign(CENTER, CENTER);
   fill(255);
@@ -99,7 +102,7 @@ function gameScreen() {
   ellipse(x, y, 40, 40);
   rect(x + 6, y - 35, 8, 30, 20);
   rect(x - 15, y - 35, 8, 30, 20);
-// eyes 
+  // eyes
   fill(255, 255, 255);
   ellipse(x + 10, y - 35, 15);
   ellipse(x - 12, y - 35, 15);
@@ -107,10 +110,10 @@ function gameScreen() {
   fill(0, 0, 0);
   ellipse(x + 10, y - 35, 8);
   ellipse(x - 12, y - 35, 8);
-// mouth 
+  // mouth
   fill(9, 121, 105);
   arc(x, y, 20, 20, 0, PI);
-//moon 
+  //moon
   fill(227, 232, 232);
   ellipse(179, 230, 150);
 
@@ -124,13 +127,13 @@ function gameScreen() {
   ellipse(226, 199, 20);
   ellipse(164, 229, 25);
 
-// other planet 
+  // other planet
   fill(194, 142, 196);
   ellipse(599, 545, 150);
   fill(229, 143, 71);
   arc(599, 545, 200, 30, 0, PI);
 
-// The little dots 
+  // The little dots
   fill(239, 218, 100);
   ellipse(103, 488, 10);
   ellipse(80, 200, 10);
@@ -156,6 +159,7 @@ function gameScreen() {
       crash = true;
     } else {
       crash = false;
+      win = true;
     }
     speedY = 0;
   }
@@ -167,6 +171,8 @@ function gameScreen() {
 
   if (crash) {
     state = "result";
+  } else if (win) {
+    state = "win";
   }
 
   // Controls spacebar
@@ -197,7 +203,7 @@ function resultScreen() {
   ellipse(360, 200, 30);
   ellipse(440, 200, 30);
 
-  // mouth 
+  // mouth
   fill(9, 121, 105);
   rect(350, 352, 110, 20, 20);
 
@@ -229,23 +235,84 @@ function resultScreen() {
     endShape(CLOSE);
   }
 
-// result text
+  // result text
   if (crash) {
     fill(9, 121, 105);
     text("You died :(", 400, 550);
   } else {
-    text("Safe Landing!", 400, 600);
+    text("Safe Landing!", 400, 550);
   }
 
   textSize(30);
   fill(9, 121, 105);
   text("Click to Restart", 400, 650);
 }
+
+function winScreen() {
+  background(69, 64, 119);
+  fill(255);
+  textSize(60);
+  textAlign(CENTER, CENTER);
+
+  text("You Win!", 400, 550);
+
+  textSize(30);
+  fill(9, 121, 105);
+  text("Click to Restart", 400, 650);
+
+  //alien
+  fill(135, 191, 116);
+  ellipse(400, 350, 200);
+  rect(348, 200, 25, 90, 20);
+  rect(430, 200, 25, 90, 20);
+
+  //eyes
+  fill(255, 255, 255);
+  ellipse(360, 200, 50);
+  ellipse(440, 200, 50);
+
+  fill(0, 0, 0);
+  ellipse(360, 200, 30);
+  ellipse(440, 200, 30);
+
+  // mouth
+  fill(9, 121, 105);
+  arc(400, 352, 100, 70, 0, PI);
+
+  // stars
+  fill(239, 218, 100);
+  drawStar(300 / 2, height / 2, 50, 10, 5);
+  drawStar(100 / 2, 100 / 2, 50, 20, 5);
+  drawStar(600 / 2, 250 / 2, 50, 10, 8);
+  drawStar(1000 / 2, 100 / 2, 50, 20, 5);
+  drawStar(1100 / 2, 400 / 2, 50, 10, 5);
+  drawStar(1400 / 2, 200 / 2, 40, 20, 5);
+  drawStar(1280 / 2, 800 / 2, 50, 10, 5);
+  drawStar(1400 / 2, 500 / 2, 40, 10, 8);
+  drawStar(200 / 2, 400 / 2, 40, 20, 5);
+
+  function drawStar(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+
+    beginShape();
+    for (let a = -PI / 2; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * radius1;
+      sy = y + sin(a + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+}
+
 function mouseClicked() {
   if (state === "start") {
     state = "game";
     resetGame();
-  } else if (state === "result") {
+  } else if (state === "result" || state === "win") {
     state = "game";
     resetGame();
   }
@@ -259,5 +326,6 @@ function resetGame() {
   gravity = 0.1;
   thrust = -0.5;
   crash = false;
+  win = false;
   isLanding = false;
 }
